936fe44bd3aeeda2452d3a8c78a84ec07723e525
SumedhNaidu <sumedhnaidu.kn@gmail.com>
2024-11-19 16:41:03 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions
+{
+    internal class Program
+    {
+    public delegate string GreetingsDelegate(string name);
+        static void Main(string[] args)
+        {
+            GreetingsDelegate obj = (name) =>
+            {
+                return "Hello @" + name + " Welcome";
+            };
+            string GreetingsMessage = obj.Invoke("Sumedh");
+            Console.WriteLine(GreetingsMessage);
+            Console.ReadKey();
+        }
+        //public static string Greetings(string name)
+        //{
+        //    return "Hello @" + name + " Wlcome";
+        //}
+    }
+}
